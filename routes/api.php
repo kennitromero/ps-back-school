0f2dd75710a0a81a82e0d51e8c933d9a4a9d117c
Kevinreyesm <kevinreyes@hotmail.com>
2023-04-03 19:33:57 -0500

    api-creacion de asignatura

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
+use App\Http\Controllers\CreateSubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/1.0/subjects', CreateSubjectController::class);
