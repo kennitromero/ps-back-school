9c278e6cf15aad796160a3f1eb65dcd6eef2284c
Abner Samuel <abnersamuelromero@gmail.com>
2023-04-03 19:29:27 -0500

    Pruebas endtoend, cru para students

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Permite obtener una lista de los estudiantes
+Route::get('/1.0/students',  [
+    StudentController::class, 'getstudents'
+]);
+
+// Permite la creacion de un estudiante
+Route::post('/1.0/students', [
+    StudentController::class, 'create'
+]);
+
+//Permite actualizar los datos de un estudiante
+route::put('/1.0/students/{studentId}', [
+    StudentController::class, 'update'
+]);
